f59a9f4314d0855bf943b3d02bc4ad01808f2d18
Jesse Henkel <jessehenkel100@gmail.com>
2025-12-16 16:54:02 +0200

    ''

--- beginner_NN_model.py
+++ beginner_NN_model.py
@@ -66,8 +66,8 @@ class node:
 
 class training_node:#used when train a model.difference to regular node is that there is only a single x and y value.use int for simplicity and faster on CPU
     def __init__(self,input):
-        self.__input = input
-        self.__output = randint(1,2^8)
+        self.__input = np.array(input).reshape(1,-1)
+        self.__output = np.array(randint(1,2^16)).reshape(1,-1)
         self.lr = LinearRegression()
         self.lr.fit(self.__input,self.__output)
     
@@ -78,12 +78,12 @@ class training_node:#used when train a model.difference to regular node is that 
         return relu(self.lr.predict(self.__input))
 
     def train(self,input):
-        self.__input = input
+        self.__input = np.array(input).reshape(1,-1)
         self.lr.fit(self.__input,self.__output)
 
     def change(self,value):
-        if self.__output > 1:
-            self.__output += int(value)
+        value = np.array(value).reshape(1,-1)
+        self.__output = np.add(self.__output,value)
 
 class text_model:
     def __init__(self,dimension,token,learning_rate):
@@ -96,9 +96,10 @@ class text_model:
             self.__token_reverse.append(key)
         #initalize model
         for num in self.__dim:
+            #use tuple for immutable and reduced memory size
             layer = [node()] * num
-            self.__layers.append(layer)
-        self.__layers = tuple(self.__layers)
+            self.__layers.append(tuple(layer))
+        self.__layers = tuple(self.__layers) 
 
     def encode(self,input):#converts text to int
         encoded_text = []
@@ -127,8 +128,13 @@ class text_model:
                 padding = [0]*(self.__dim[0] - len(input))
             elif len(input) > self.__dim[0]:raise Warning('input size is larger than context window')
             input.extend(padding)
+            #pad output so that it is same size as input layer
+            if len(output) < self.__dim[0]:
+                padding = [0]*(self.__dim[0] - len(output))
+            elif len(output) > self.__dim[0]:raise Warning('input size is larger than context window')
+            output.extend(padding)
             data_forward = 0#sum of all wieghts
-            data_layer = []#stores the the individual data of the prev layer
+            data_layer = []#stores the the individual data of the output layer for calculating loss
             training_layers = []
 
             #initiat with first layer
@@ -149,7 +155,6 @@ class text_model:
                     layer.append(node)
                 training_layers.append(layer)
                 data_forward = data_forward_next
-            if len(data_forward) != len(self.__dim[-1]): raise Warning(f"size of output is not the same zise as output layer\noutput size-{len(data_forward)}\noutput layer size-{len(self.__dim[-1])}")
 
             #output layer and forward pass
             for n in range(self.__dim[-1]):
@@ -157,50 +162,53 @@ class text_model:
                 data_layer.append(node.output())
                 layer.append(node)
             training_layers.append(layer)
+            if len(data_layer) != self.__dim[-1]: raise Warning(f"size of output is not the same size as output layer\noutput size:{len(data_layer)}\noutput layer size:{self.__dim[-1]}")
+            elif len(output) != self.__dim[-1]: raise Warning(f"size of output is not the same size as output layer\noutput size:{len(output)}\noutput layer size:{self.__dim[-1]}")
 
             #calc loss function
             avg_loss = 0
-            for n in range(len(self.__dim[-1])):
+            for n in range(self.__dim[-1]):
                 avg_loss += output[n] - data_layer[n]
-            avg_loss = avg_loss / len(self.__dim[-1])
+            avg_loss = avg_loss / self.__dim[-1]
+            print(f"initial loss:{avg_loss}")
             loss_list = [avg_loss]
             avg_loss_next = 0
 
             #forward pass training
             data_forward = 0
             data_forward_training = 0
-            direction = 1#if positive node is increased and visa versa
+            direction = 1#if positive node output value is increased and visa versa
             loss = 0
 
             #input layer
-            for node_num in range(len(self.__dim[0])):
+            for node_num in range(self.__dim[0]):
                 for _ in range(10):
                     change_amount = avg_loss * self.__learning_rate * direction
                     node = training_layers[0][node_num]
                     node.change(change_amount)
 
                     #forward pass input after change
-                    for n in range(len(self.__dim[0])):
+                    for n in range(self.__dim[0]):
                         training_layers[0][n].train(input[n])
                         data_forward_training += training_layers[0][n].output()
                     for l in range(layer_num,len(self.__dim)-1):
                         data_forward_training_next = 0
-                        for n in range(len(self.__dim[l])):
+                        for n in range(self.__dim[l]):
                             training_layers[l][n].train(data_forward_training)
                             data_forward_training_next += training_layers[l][n].output()
                         data_forward_training = data_forward_training_next
                     data_forward_training_next = 0
                     data_layer = []
-                    for n in range(len(self.__dim[-1])):
+                    for n in range(self.__dim[-1]):
                         training_layers[-1][n].train(data_forward_training)
                         data_layer.append(training_layers[l][n].output())
                         data_forward_training_next += training_layers[-1][n].output()
 
-                    for n in range(len(self.__dim[-1])):#calc new loss
+                    for n in range(self.__dim[-1]):#calc new loss
                             loss += output[n] - data_layer[n]
                             if loss < 0:loss*-1#convert to positive value
                             avg_loss_next += loss
-                    avg_loss_next = avg_loss_next / len(self.__dim[-1])
+                    avg_loss_next = avg_loss_next / self.__dim[-1]
                     if 0 <= avg_loss-avg_loss_next <= 0.1: break#stop training when gradient is small
                     elif avg_loss > avg_loss_next:direction = 1
                     else: direction = -1
@@ -208,12 +216,13 @@ class text_model:
                     avg_loss_next = 0
                 avg_loss = avg_loss_next
                 loss_list.append(avg_loss)
+                print(f"New loss:{avg_loss}")
                 avg_loss_next = 0
                 
                 data_forward += node.output()
             #hidden layer and output
             for layer_num in range(1,len(self.__dim)):
-                for node_num in range(len(self.__dim[layer_num])):
+                for node_num in range(self.__dim[layer_num]):
                     for _ in range(10):
                         change_amount = avg_loss * self.__learning_rate * direction
                         node = training_layers[layer_num][node_num]
@@ -221,19 +230,19 @@ class text_model:
                         #calc change
                         for l in range(layer_num,len(self.__dim)-1):
                             data_forward_training_next = 0
-                            for n in range(len(self.__dim[l])):
+                            for n in range(self.__dim[l]):
                                 training_layers[l][n].train(data_forward_training)
                                 data_forward_training_next += training_layers[l][n].output()
                         data_layer = []
-                        for n in range(len(self.__dim[-1])):
+                        for n in range(self.__dim[-1]):
                             training_layers[-1][n].train(data_forward_training)
                             data_layer.append(training_layers[l][n].output())
                             data_forward_training_next += training_layers[-1][n].output()
-                        for n in range(len(self.__dim[-1])):#calc new loss
+                        for n in range(self.__dim[-1]):#calc new loss
                             loss += output[n] - data_layer[n]
                             if loss < 0:loss*-1#convert to positive value
                             avg_loss_next += loss
-                        avg_loss_next = avg_loss_next / len(self.__dim[-1])
+                        avg_loss_next = avg_loss_next / self.__dim[-1]
                         if 0 <= avg_loss-avg_loss_next <= 0.1: break#stop training when gradient is small
                         elif avg_loss > avg_loss_next:direction = 1
                         else: direction = -1
@@ -241,9 +250,22 @@ class text_model:
                         avg_loss_next = 0
                     avg_loss = avg_loss_next
                     loss_list.append(avg_loss)
+                    print(f"New loss:{avg_loss}")
                     avg_loss_next = 0
                     data_forward_next += node.output()
                 data_forward = data_forward_next
+
+                mpl.figure()
+                mpl.plot(loss_list)
+                mpl.title('loss graph')
+                mpl.xlabel(layer)
+                mpl.ylabel('loss')
+                mpl.show()
+                
+                #append points to 
+                for layer_num in self.__dim:
+                    for n in self.__dim[layer_num]:
+
                 
         with open('custom_nn.py','w') as model:
             #store model as an python script that is executed via terminal.
@@ -263,4 +285,4 @@ token_words = {}
 token_words = tokenize(token_words,df)
 model = text_model((50,10,10,50),token_words,0.1)
 for text, code in df[['Text', 'Code']].values:
-    model.train(str(text), str(code))
+    model.train(text, code)
